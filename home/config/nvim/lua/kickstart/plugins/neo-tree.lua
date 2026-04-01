48641f9ec827f238cdd4f3fb2b613b9d3d479b12
Pavel Varonin <pavel.v@getblock.io>
2026-04-01 12:02:47 +0400

    tweaks

--- home/config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ home/config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -1,26 +1,27 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  version = "*",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-  },
-  cmd = "Neotree",
-  keys = {
-    { "<leader>n", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
-  },
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        hide_dotfiles = false,
-        hide_gitignored = false,
-      },
-      window = {
-        mappings = {
-          ["<leader>n"] = "close_window",
-        },
-      },
-    },
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+	},
+	cmd = "Neotree",
+	keys = {
+		{ "<leader>n", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
+	},
+	opts = {
+		filesystem = {
+			filtered_items = {
+				visible = true,
+				hide_dotfiles = false,
+				hide_gitignored = false,
+			},
+			window = {
+				mappings = {
+					["<leader>n"] = "close_window",
+				},
+			},
+		},
+	},
 }
